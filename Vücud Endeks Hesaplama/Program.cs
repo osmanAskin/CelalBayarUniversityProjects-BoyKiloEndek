f969713d15d46529b440e6cd64f8a8b2b983ad44
Osman Orhan Askin <115871580+osmanAskin@users.noreply.github.com>
2023-11-08 20:01:43 +0300

    update

---
+++ Vücud Endeks Hesaplama/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Vücud_Endeks_Hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kilonuzu Giriniz: ");
+            int kilo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Boyunuzu Giriniz (örnek 1.82): ");
+            double boy = double.Parse(Console.ReadLine());
+
+            double bki = kilo / (boy * boy);
+
+            Console.WriteLine("Beden Kitle İndeksiniz: " + bki);
+
+            Console.ReadLine(); // Programın kapanmasını önlemek için bu satırı ekledim.
+        }
+    }
+}
